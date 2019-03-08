ea4d6ff751dcd2922484dd51e053a72f359532c6
hardik <jayanihardik@gmaill.com>
2019-03-08 17:45:19 +0530

    remove package  ironmongers

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
-const mongoDBErrors = require("mongoose-mongodb-errorS")
-mongoose.plugin(mongoDBErrors)
 mongoose.connect(process.env.MONURL ? process.env.MONURL: `mongodb://hardik:admin123456@ds161497.mlab.com:61497/gim`,{ useNewUrlParser: true })
 
